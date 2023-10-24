6c942a25b6a7521b067f4547c13fccdcc7ba90c0
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2023-10-24 16:11:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const mp3List = document.getElementById('mp3-list');
-const audioPlayer = document.getElementById('audio-player');
-const audioSource = document.getElementById('audio-source');
 
-mp3List.addEventListener('click', e => {
-  e.preventDefault(); // prevent default link behavior
-  const mp3Url = e.target.href;
-  audioSource.src = mp3Url;
-  audioPlayer.load();
-  audioPlayer.play();
-});
